3760263ddadea97174b1e32eccc0cd28d7ea03c0
Leonardo Palavecino <leo43161301gmail.com>
2021-01-03 02:23:54 -0300

    Se agrego bootstrap y se comprobo su funcionamient

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "react-bootstrap/Card"
+
+const Login = () => {
+  return (
+    <div>
+      <Card>
+        <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
