b2e5588744fab88991ec9b508d8416cbd03af331
Leonardo Palavecino <leo43161301gmail.com>
2021-01-04 02:38:21 -0300

    Se agrego al git ignore el cache

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
+      <Home></Home>
       <Switch>
         <Route exact path="/login">
           <Login></Login>
