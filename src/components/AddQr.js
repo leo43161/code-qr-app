d5c8964b84c5d974b3d44612e04ce8b5cd34743c
Leonardo <leo43161301@gmail.com>
2021-01-17 10:09:24 -0300

    Se reemplazo el diseño anterior por uno mas responsive

--- src/components/AddQr.js
+++ src/components/AddQr.js
@@ -41,87 +41,6 @@ const AddQr = () => {
 
   return (
     <div className="container-fluid p-0">
-      <div className="p-3 bg-dark">
-        <img
-          src={process.env.PUBLIC_URL + "/img/codigo-qr.png"}
-          className="img-fluid"
-          alt="qr-img"
-        />
-        <div className="mt-2">
-          <h4 className="text-center text-light">Compartir</h4>
-          <div className="d-flex justify-content-around">
-            <Button variant="light" disabled={false}>
-              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-            </Button>
-            <Button variant="light" disabled={false}>
-              <FontAwesomeIcon icon={faFacebookF} size="2x" />
-            </Button>
-            <Button variant="light" disabled={false}>
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
-            </Button>
-            <Button variant="light" disabled={false}>
-              <FontAwesomeIcon icon={faArrowDown} size="2x" />
-            </Button>
-          </div>
-        </div>
-      </div>
-      <div className="p-3">
-        <h3 className="text-center">Informacion</h3>
-        <p>
-          <small>
-            (<span className="text-danger">*</span>) Los campos son
-            obligatorios.
-          </small>
-        </p>
-        <div>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>
-                Título <span className="text-danger">*</span>
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ej: Cerveza 2x1"
-                required
-                name="titulo"
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Destinatario</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ej: Sara Gonzalez"
-                name="destinatario"
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>
-                Descripción <span className="text-danger">*</span>
-              </Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder=""
-                required
-                name="descripcion"
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {error ? (
-              <Alert variant="warning">
-                No llenaste todos los campos obligatorios (
-                <span className="text-danger">*</span>)
-              </Alert>
-            ) : null}
-
-            <Button variant="dark" className="w-100" type="submit">
-              Crear
-            </Button>
-          </Form>
-        </div>
-      </div>
       <div className="py-md-5 p-3 bg-dark w-100 d-flex flex-md-row align-items-md-center justify-content-md-around flex-column">
         <div className="col-md-5 p-0 text-md-center">
           <img
@@ -164,7 +83,6 @@ const AddQr = () => {
                     Título <span className="text-danger">*</span>
                   </Form.Label>
                   <Form.Control
-                    className="text-light"
                     type="text"
                     placeholder="Ej: Cerveza 2x1"
                     required
